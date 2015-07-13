59b5a48ed4c2a4847f01f6f5a9876b82953e9846
Yann Régis-Gianas <yrg@pps.univ-paris-diderot.fr>
2015-07-13 11:21:52 +0200

    src/mtac2Instr.mli: Comment.

--- src/mtac2Instr.mli
+++ src/mtac2Instr.mli
@@ -1,4 +1,6 @@
+(** This module defines the abstract syntax tree for Mtac2 instructions. *)
 
+(** Mtac2 uses the syntax of Coq to represent programs. *)
 type mproof_instr =
   | Run of Constrexpr.constr_expr
 
