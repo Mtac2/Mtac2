87b7a4d9b1fb30765d553391d1eb7f7d691b7d01
Jacques-Pascal Deplaix <jp.deplaix@gmail.com>
2016-01-18 18:53:57 +0100

    Make the call_ltac type parameter implicit

--- theories/MetaCoq.v
+++ theories/MetaCoq.v
@@ -120,7 +120,7 @@ Inductive MetaCoq : Type -> Prop :=
 
 | munify {A} (x y : A) : MetaCoq (x = y)
 
-| call_ltac : forall A, string -> MetaCoq A
+| call_ltac : forall {A : Type}, string -> MetaCoq A
 | list_ltac : forall {A : Type} {_ : A}, MetaCoq A
 .
 
