479f207aad1b394b064783aa95e8e4a10cd41be3
Jan-Oliver Kaiser <janno@mpi-sws.org>
2016-07-10 10:05:36 +0200

    got some examples to work (with ltac, not MetaCoq)

--- theories/DepDestruct.v
+++ theories/DepDestruct.v
@@ -10,12 +10,12 @@ Import ListNotations.
 
 Inductive Bla : Prop := Ble.
 
-Inductive Sort := SType | SProp.
+Polymorphic Inductive Sort := SType | SProp.
 Polymorphic Definition stype_of s := match s with SType => Type | SProp => Prop end.
 Polymorphic Definition selem_of {s} (x : stype_of s) : Type :=
   match s return stype_of s -> Type with
-  | SType => id
-  | SProp => id
+  | SType => fun x => x
+  | SProp => fun x => x
   end x.
 
 Polymorphic Inductive ITele (sort : Sort) : Type :=
@@ -91,7 +91,7 @@ Polymorphic Fixpoint ITele_App {sort} {it : ITele sort} (args : ATele it) : styp
 
 Example reflect_app P b := Eval compute in ITele_App (reflect_args P b).
 
-Definition type_of {A} (x : A) := A.
+Polymorphic Definition type_of {A} (x : A) := A.
 
 (* We need to handle Prop (maybe) *)
 Polymorphic Fixpoint abstract_goal {isort} {rsort} {it : ITele isort} (args : ATele it) (G : stype_of rsort) :
@@ -141,71 +141,95 @@ Polymorphic Fixpoint get_type_of_branch {isort} {rsort} {it : ITele isort} (ct :
       end v rec
   end.
 
+Polymorphic Definition ForAll {sort} {A} : (A -> stype_of sort) -> stype_of sort :=
+  match sort with
+  | SProp => fun F => forall (a : A), F a
+  | SType => fun F => forall (a : A), F a
+  end.
 
-(*
-Polymorphic Definition get_type_of_branch {it : ITele} (rt : RTele it) (ct : CTele it) : Type.
-Proof.
-  induction ct as [T t|T f t ct' IH|T it f IH].
-  - inversion rt.
-    exact t1.
-  - inversion rt.
-    rewrite <- (Eqdep.EqdepTheory.inj_pair2 _ _ _ _ _ H1) in IH.
-    exact (IH (X t)).
-  - exact (forall x, IH x rt).
-Defined.
-*)
+Polymorphic Definition Fun {sort} {A} :
+  forall {F : A -> stype_of sort}, (forall a, selem_of (F a)) -> selem_of (ForAll F) :=
+  match sort as sort' return
+  forall {F : A -> stype_of sort'}, (forall a, selem_of (F a)) -> selem_of (ForAll F)
+  with
+  | SProp => fun _ f => f
+  | SType => fun _ f => f
+  end.
+
+Polymorphic Fixpoint RTele_Type {isort} {it : ITele isort} {rsort} (rt : RTele rsort it) : Type :=
+  match rt with
+  | @rBase _ _ s r =>
+    (forall (t : selem_of s), stype_of rsort) : Type
+  | rTele rt => forall t, RTele_Type (rt t)
+  end.
+
+Polymorphic Fixpoint RTele_Fun {isort} {it : ITele isort} {rsort} (rt : RTele rsort it) : RTele_Type rt :=
+  match rt with
+  | rBase r => r
+  | rTele rt => fun t => (RTele_Fun (rt t))
+  end.
 
 Example bla P : RTele _ (reflect_reflect P) :=
   Eval simpl in rTele (fun b=>rBase (rsort:=SProp) (fun _=>P <-> b = true)).
 Example bla_branch P := Eval simpl in get_type_of_branch (reflect_RTrue P) (bla P).
 
-Definition new_destruct_goals {i : IndType} (g : RTele (ind_type i)) :=
-  map (fun cs => get_type_of_branch g cs) (ind_constructors i).
+Definition new_destruct_goals {isort} {it : ITele isort} {rsort} (g : RTele rsort it) :=
+  map (fun ct => get_type_of_branch ct g).
 
 
-Example bla_RTele P b :=
-  Eval compute in eval (abstract_goal (reflect_args P b) ((P <-> b = true))).
+Example bla_RTele P b (r : reflect P b) :=
+  Eval compute in eval (abstract_goal (rsort := SProp) (reflect_args P b) ((P <-> b = true)) r).
 
-Example bla_goals P b : list dyn :=
+Example bla_goals P b r : list dyn :=
   Eval compute in
-    map (fun cs => Dyn (get_type_of_branch (bla_RTele P b) cs))
+    map (fun cs => Dyn (get_type_of_branch (rsort := SProp) cs (bla_RTele P b r)))
         (reflect_RTrue P :: reflect_RFalse P :: nil).
 
-Example reflectP_it : ITele :=
-  iTele (fun P => iTele (fun b => iBase (reflect P b))).
+Example reflectP_it : ITele _ :=
+  iTele (fun P => iTele (fun b => iBase (sort := SType) (reflect P b))).
 Example reflectP_RTrue : CTele reflectP_it :=
-  cProd (fun P => cProd (fun p => cInst P (cInst true (cBase (@RTrue P p))))).
+  cProd (fun P => cProd (fun p => cInst P (cInst true (cBase (sort := SType) (@RTrue P p))))).
 Example reflectP_RFalse : CTele reflectP_it :=
-  cProd (fun P => cProd (fun p => cInst P (cInst false (cBase (@RFalse P p))))).
+  cProd (fun P => cProd (fun p => cInst P (cInst false (cBase (sort := SType) (@RFalse P p))))).
 Example reflectP_args P b : ATele reflectP_it :=
   aTele P (aTele b (aBase)).
 
-Example blaP_RTele P b :=
-  Eval compute in eval (abstract_goal (reflectP_args P b) ((P <-> b = true))).
+Example blaP_RTele P b r :=
+  Eval compute in eval (abstract_goal (rsort := SProp) (reflectP_args P b) ((P <-> b = true)) r).
 
-Example blaP_goals P b : list dyn :=
+Example blaP_goals P b r : list dyn :=
   Eval compute in
-    map (fun cs => Dyn (get_type_of_branch (blaP_RTele P b) cs))
-        (reflectP_RTrue :: reflectP_RFalse :: nil).
+    map (fun cs => Dyn (get_type_of_branch cs (blaP_RTele P b r)))
+        (reflectP_RFalse :: reflectP_RTrue :: nil).
 
-Polymorphic Definition RTele_App : forall {it}, RTele it -> ATele it -> Type :=
-  fix rec it r :=
-    match r with
-    | rBase t => fun _ => t
-    | @rTele T f r =>
-      let rec' := fun t => rec _ (r t) in
-      fun a : ATele (iTele f) =>
-        match a in ATele it' return
-              match it' with
-              | iBase _ => True
-              | iTele f => _ -> _
-              end
-        with
-        | aBase => I
-        | aTele t a => fun rec => rec t a
-        end rec'
-    end
-.
+Polymorphic Fixpoint RTele_App {isort rsort} {it : ITele isort} (rt : RTele rsort it) : forall (a : ATele it), selem_of (ITele_App a) -> stype_of rsort :=
+  match rt in RTele _ it'
+  with
+  | @rBase _ _ T t =>
+    fun (a : ATele (iBase T)) =>
+      match a as a' in ATele it' return
+            match it' with
+            | iBase T' => (selem_of T' -> stype_of rsort) -> selem_of (ITele_App a') -> stype_of rsort
+            | iTele f => True
+            end
+      with
+      | aBase => fun f => f
+      | aTele _ _ => I
+      end t
+  | @rTele _ _ _ f r =>
+    let rec t := RTele_App (r t) in
+    fun (a : ATele (iTele f)) =>
+      match a as a' in ATele it' return
+            match it' with
+            | iBase _ => True
+            | @iTele _ T' f => (forall (t:T') (a:ATele (f t)), selem_of (ITele_App a) -> _) -> selem_of (ITele_App a') -> stype_of rsort
+            end
+      with
+      | aBase => I
+      | aTele v a => fun rec => rec v a
+      end rec
+  end.
+
 
 Goal True.
 MProof.
@@ -218,25 +242,64 @@ MProof.
   cpose case (fun y=>idtac) g) : tactic.
 Abort.
 
-Require Import Vector.
-
-Goal forall n (v : t nat n), n = length (to_list v).
-
 Goal forall P b, reflect P b -> P <-> b = true.
-MProof.
-  intros P b.
-  (fun g=>
-   G <- goal_type g;
-   rG <- abstract_goal (reflectP_args P b) G;
-   pose (rG := rG) g) : tactic.
-  select (reflect _ _) (fun x=>
-  case <- makecase {|
-    case_val := x;
-    case_type := RTele_App (
-;
-  cpose case (fun y=>idtac) g) : tactic.
-Abort.
+Proof.
+  intros P b r.
+  pose (rG := eval (abstract_goal (rsort := SType) (reflect_args P b) (P <-> b = true) r)).
+  simpl in rG.
+  assert (T : get_type_of_branch (reflect_RTrue P) rG).
+  { now firstorder. }
+  assert (F : get_type_of_branch (reflect_RFalse P) rG).
+  { compute. firstorder. now discriminate. }
+  pose (mc :=
+          makecase {|
+              case_val := r;
+              case_type := P <-> b = true;
+              case_return := Dyn (RTele_Fun rG);
+              case_branches := (Dyn T) :: (Dyn F) :: nil
+            |}).
+  unfold blaP_goals in mc.
+  compute in mc.
+  pose (c := eval mc).
+  exact (elem c).
+Qed.
 
+
+Section VectorExample.
+Require Import Vector.
+Goal forall n (v : t nat n), n = length (to_list v).
+Proof.
+  pose (it := iTele (fun n => @iBase SType (t nat n))).
+  pose (vnil := (cInst 0 (@cBase SType _ (nil nat))) : CTele it).
+  pose (vcons := (cProd (fun a => cProd (fun n => cProd (fun (v : t nat n) => cInst (S n) (@cBase SType _ (cons _ a _ v)))))) : CTele it).
+  fix f 2.
+  intros n v.
+  pose (a := (aTele n (aBase)) : ATele it).
+  pose (rt := eval (abstract_goal (rsort := SProp) a (n = length (to_list v)) v)).
+  simpl in vcons, rt.
+  assert (N : get_type_of_branch vnil rt).
+  { now auto. }
+  assert (C : get_type_of_branch vcons rt).
+  { intros x k v'. cbv. f_equal. exact (f _ _). }
+  pose (mc :=
+          makecase {|
+              case_val := v;
+              case_type := n = length (to_list v);
+              case_return := Dyn (RTele_Fun rt);
+              case_branches := Dyn N :: Dyn C :: List.nil
+            |}
+       ).
+  simpl RTele_Fun in mc.
+  (* pose (ma := (match v as v' in t _ k return k = length (to_list v') with *)
+  (*              | nil _ => N *)
+  (*              | cons _ a k v => C a k v *)
+  (*              end)). *)
+  (* pose (c' := eval (destcase ma)). *)
+  (* unfold eval in c'. *)
+  pose (c := eval mc).
+  exact (elem c).
+Qed.
+End VectorExample.
 
 Definition new_destruct {A : Type} (n : A) : tactic := fun g=>
   b <- is_var n;
