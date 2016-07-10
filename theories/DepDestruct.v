79b2329c9db4da8b141d61829aa180f1502a7280
Jan-Oliver Kaiser <janno@mpi-sws.org>
2016-07-10 11:04:13 +0200

    whatever case_type is, it's now computed automatically

--- theories/DepDestruct.v
+++ theories/DepDestruct.v
@@ -254,7 +254,7 @@ Proof.
   pose (mc :=
           makecase {|
               case_val := r;
-              case_type := P <-> b = true;
+              case_type := RTele_App rG (reflect_args P b) r;
               case_return := Dyn (RTele_Fun rG);
               case_branches := (Dyn T) :: (Dyn F) :: nil
             |}).
@@ -284,7 +284,7 @@ Proof.
   pose (mc :=
           makecase {|
               case_val := v;
-              case_type := n = length (to_list v);
+              case_type := RTele_App rt a v;
               case_return := Dyn (RTele_Fun rt);
               case_branches := Dyn N :: Dyn C :: List.nil
             |}
