155eae51ec53e25e5430fbbec2cb575b71787ca7
Beta Ziliani <bziliani@famaf.unc.edu.ar>
2016-07-11 13:44:31 +0200

    tests for ltac and goals. some are failing and we need to fix them

---
+++ tests/ltac.v
@@ -0,0 +1,62 @@
+From MetaCoq
+Require Import MCTactics ImportedTactics.
+
+Require Import Bool.Bool.
+Require Import Lists.List.
+
+Import ListNotations.
+Import MetaCoqNotations.
+Import MCTacticsNotations.
+
+Ltac induction n := induction n.
+
+Definition qualify s := String.append "Top." s.
+
+Definition induction {A} (n:A) : tactic := ltac (qualify "induction") [Dyn n].
+
+Goal forall n:nat, 0 <= n.
+MProof.
+  intros.
+  induction n;; [apply le_n; apply le_S;; assumption].
+Qed.
+
+
+Goal forall m n:nat, 0 <= n.
+MProof.
+  intros.
+  (* m shouldn't be in the list of hypotheses, as it is shared *)
+  (fun g=>r <- induction n g;
+   match r with
+   | (TheGoal _ :: _) => ret r
+   | _ => raise exception
+   end) ;; [apply le_n; apply le_S;; assumption].
+Qed.
+
+Ltac myapply H := apply H.
+Definition apply' := qualify "myapply".
+
+Ltac remove H := clear H.
+Definition remove := qualify "remove".
+
+Goal forall P Q, (P -> Q) -> P -> Q.
+MProof.
+  intros P Q f x.
+  apply f.
+  (* WHAT? it is not closing the goal with x, although it should *)
+  (fun g=>r <- ltac remove [Dyn f] g; print_term r;; ret r):tactic.
+Abort.
+
+Goal forall P Q, (P -> Q) -> P -> Q.
+MProof.
+  intros P Q f x.
+  Fail select (_->_) (fun g=>ltac apply' [Dyn g]). (* WHAT?? It says  "Unknown existential variable" *)
+Abort.
+
+Goal forall P Q, (P -> Q) -> P -> Q.
+MProof.
+  intros P Q.
+  (* with indices it should work too *)
+  Fail cintros f x {-
+    ltac apply' [Dyn f];; ltac apply' [Dyn x]
+  -}.
+Abort.
