8e4a483f62b597f47891839b9007a0a90fb75318
Jan-Oliver Kaiser <janno@mpi-sws.org>
2016-07-22 14:16:09 +0200

    falling off the edge of the universe

--- theories/DepDestruct.v
+++ theories/DepDestruct.v
@@ -8,20 +8,25 @@ Require Import Strings.String.
 Require Import Lists.List.
 Import ListNotations.
 
-Polymorphic Definition type_of {A} (x : A) := A.
+Set Printing Universes.
 
-Polymorphic Inductive Sort := SType | SProp.
-Polymorphic Definition stype_of s := match s with SType => Type | SProp => Prop end.
-Polymorphic Definition selem_of {s} (x : stype_of s) : Type :=
-  match s return stype_of s -> Type with
-  | SType => fun x => x
-  | SProp => fun x => x
-  end x.
+Section Sorts.
+  Inductive Sort : Type := SProp | SType.
+  Polymorphic Definition type_of@{i} {A : Type@{i}} (x : A) : Type@{i} := A.
+  Polymorphic Definition stype_of@{i j} (s : Sort) : Type@{j}
+    := match s with SType => Type@{i} | SProp => Prop end.
+  Polymorphic Definition selem_of@{i j} {s : Sort} (x : stype_of@{i j} s) : Type@{i} :=
+    match s return stype_of s -> Type@{i} with
+    | SType => fun x => x
+    | SProp => fun x => x
+    end x.
+End Sorts.
 
-Polymorphic Definition ForAll {sort} {A} : (A -> stype_of sort) -> stype_of sort :=
-  match sort with
-  | SProp => fun F => forall (a : A), F a
-  | SType => fun F => forall (a : A), F a
+Polymorphic Definition ForAll@{i j} {sort : Sort} {A : Type@{i}}
+  : (A -> stype_of@{i j} sort) -> stype_of sort :=
+  match sort as sort' return (A -> stype_of@{i j} sort') -> stype_of sort'  with
+  | SProp => fun (F : A -> Prop) => (forall (a : A), F a) : stype_of SProp
+  | SType => fun (F : A -> Type@{i}) => (forall (a : A), F a) : stype_of SType
   end.
 
 Polymorphic Definition Fun {sort} {A} :
@@ -39,17 +44,16 @@ Polymorphic Definition App {sort} {A} : forall {F : A -> _},  selem_of (ForAll (
   | SType => fun F f a => f a
   end.
 
-
-Polymorphic Inductive ITele (sort : Sort) : Type :=
-| iBase : stype_of sort -> ITele sort
-| iTele : forall {T}, (T -> ITele sort) -> ITele sort.
+Polymorphic Inductive ITele@{inner inner_1 outer} (sort : Sort) : Type@{outer} :=
+| iBase : stype_of@{inner inner_1} sort -> ITele sort
+| iTele : forall {T : Type@{inner_1}}, (T -> ITele sort) -> ITele sort.
 
 Arguments iBase {_} _.
 Arguments iTele {_ _} _.
 
-Polymorphic Inductive ATele {sort} : ITele sort -> Type :=
-| aBase : forall {T: stype_of sort}, ATele (iBase T)
-| aTele : forall {T f} (a:T), ATele (f a) -> ATele (iTele f).
+Polymorphic Inductive ATele@{i j k l} {sort} : ITele@{i j k} sort -> Type@{l} :=
+| aBase : forall {T: stype_of@{i j} sort}, ATele (iBase T)
+| aTele : forall {T : Type@{j}} {f : T -> ITele@{i j k} sort} (a:T), ATele (f a) -> ATele (iTele f).
 
 Arguments aBase {_ _}.
 Arguments aTele {_ _ _} _ _.
@@ -138,7 +142,7 @@ Inductive reflect (P :Prop) : bool -> Type :=
 | RTrue : P -> reflect P true
 | RFalse : ~P -> reflect P false.
 
-Example reflect_reflect P : ITele SType := iTele (fun b=>@iBase SType (reflect P b)).
+Example reflect_reflect P : ITele (SType) := iTele (fun b=>@iBase SType (reflect P b)).
 
 Example reflect_RTrue P : CTele (reflect_reflect P) :=
   (cProd (fun p=>@cBase SType _ (aTele _ (aBase)) (RTrue P p))).
@@ -293,7 +297,7 @@ Module VectorExample.
 Require Import Vector.
 Goal forall n (v : t nat n), n = length (to_list v).
 Proof.
-  pose (it := iTele (fun n => @iBase SType (t nat n))).
+  pose (it := iTele (fun n => @iBase (SType) (t nat n))).
   pose (vnil := ((@cBase SType _ (aTele 0 aBase) (nil nat))) : CTele it).
   pose (vcons := (cProd (fun a => cProd (fun n => cProd (fun (v : t nat n) => (@cBase SType _ (aTele (S n) aBase) (cons _ a _ v)))))) : CTele it).
   fix f 2.
@@ -326,36 +330,41 @@ Proof.
 Qed.
 End VectorExample.
 
-Definition get_ITele : forall {T : Type} (ind : T), M ({s : Sort & ITele s}) :=
-mfix2 f (T : _) (ind : _) : M _ :=
+Polymorphic Definition get_ITele : forall {T : Type} (ind : T), MetaCoq (nat * {s : Sort & ITele s}) :=
+mfix2 f (T : _) (ind : _) : M (nat * {s : Sort & ITele s})%type :=
+  print_term ind;;
   mmatch T with
   | [? (A : Type) (F : A -> Type)] forall a, F a =>
     [H]
         let indFun := match H in eq _ P return P with eq_refl => ind end
                      in nu a : A,
-                               r <- f (F a) (indFun a) : M ({s : Sort & ITele s});
-                     let (sort, it) := r in
+                               r <- f (F a) (indFun a);
+                     let (n, sit) := r in
+                     let (sort, it) := sit : {s : Sort & ITele s} in
                      f <- abs a it;
-                       ret (existT _ sort (iTele f))
+                       ret (S n, existT _ sort (iTele f))
   | Prop =>
    [H]
       let indProp := match H in eq _ P return P with eq_refl => ind end
-                    in ret (existT _ SProp (iBase (sort := SProp) indProp))
+                    in ret (0, existT _ SProp (iBase (sort := SProp) indProp))
   | Type =>
     [H]
        let indType := match H in eq _ P return P with eq_refl => ind end
-                      in ret (existT _ SType (iBase (sort := SType) indType))
+                      in ret (0, existT _ (SType) (iBase (sort := SType) indType))
   | Set =>
     [H]
        let indType := match H in eq _ P return P with eq_refl => ind end
-                      in ret (existT _ SType (iBase (sort := SType) indType))
-            end.
+                      in ret (0, existT _ (SType) (iBase (sort := SType) indType))
+                    | _ => failwith "Impossible ITele"
+           end
+                      .
 
-Example get_reflect_ITele := Eval compute in eval (get_ITele (reflect True)).
-Example reflect_sort := Eval compute in let (sort, _) := get_reflect_ITele in sort.
+Example get_reflect_ITele := Eval compute in ltac:(mrun (get_ITele (reflect True))).
+Example reflect_nindx := Eval compute in let (n, _) := get_reflect_ITele in n.
+Example reflect_sort := Eval compute in let (sort, _) := snd get_reflect_ITele in sort.
 Example reflect_itele : ITele reflect_sort :=
   Eval compute in
-  match get_reflect_ITele as pair return let (sort, _) := pair in ITele sort with
+  match snd get_reflect_ITele as pair return let (sort, _) := pair in ITele sort with
   | existT _ s it => it
   end.
 
@@ -381,10 +390,10 @@ Fixpoint args_of_max (max : nat) : forall {A}, A -> M (list dyn) :=
 Example args_of_RTrue P := Eval compute in ltac:(mrun (args_of _ (reflect P true))).
 Example args_of_max_RTrue P := Eval compute in ltac:(mrun (args_of_max 2 (reflect P true))).
 
-Fixpoint get_ATele {isort} : forall (it : ITele isort), list dyn -> M (ATele it) :=
+Polymorphic Fixpoint get_ATele {isort} : forall (it : ITele isort), list dyn -> M (ATele it) :=
   fix rec it al :=
     match it as it', al return M (ATele it') with
-    | iBase T, nil => ret aBase
+    | iBase T, nil => ret (@aBase _ T)
     | iTele f, t_dyn :: al =>
       t <- coerce (elem t_dyn);
         r <- rec (f t) al;
@@ -394,75 +403,148 @@ Fixpoint get_ATele {isort} : forall (it : ITele isort), list dyn -> M (ATele it)
 
 Example ATele_of_RTrue := Eval compute in ltac:(mrun (get_ATele (reflect_itele) (tail (args_of_RTrue True)))).
 
-Definition get_CTele_raw : forall {isort} (it : ITele isort) (nindx : nat) {A : stype_of isort}, selem_of A -> M (CTele it) :=
+Polymorphic Definition get_CTele_raw : forall {isort} (it : ITele isort) (nindx : nat) {A : stype_of isort}, selem_of A -> M (CTele it) :=
   fun isort it nindx =>
     mfix2 rec (A : stype_of isort) (a : selem_of A) : M (CTele it) :=
+    print "get_CTele_raw: A";;
     print_term A;;
-    mmatch A return M (CTele it) with
-    | [? (B : Type) (F : B -> stype_of isort)] ForAll (sort := isort) F =>
-      [H]
-        print "Prod case";; print_term F;;
-        let f := match H in _ = P return selem_of P with eq_refl => a end in
-                nu a : B,
-                       r <- rec (F a) (App f a);
-                   f' <- abs a r;
+               B <- evar Type;
+      F <- evar (B -> stype_of isort);
+      oH <- munify A (ForAll F) UniNormal;
+      match oH with
+      | Some H =>
+        print "Prod case";;
+        let f := reduce RedWhd (match H in _ = P return selem_of P with eq_refl => a end) in
+                nu b : B,
+                       r <- rec (F b) (App f b);
+                   f' <- abs b r;
+                   print "After Abs";;
                    ret (cProd f')
-    | _ => print "NoFun case";;
-                 print_term A;;
-                 let A_red := hnf A in
-                 print_term A_red;;
-                 args <- args_of_max nindx A_red;
-             print_term args;;
-             atele <- get_ATele it args;
-             print_term (ITele_App atele);;
-             print_term (a);;
-             a' <- @coerce _ (selem_of (ITele_App (isort := isort) atele)) a ;
-             ret (cBase atele a')
+      | None =>
+        m1 <- munify B (stype_of isort) UniNormal;
+          match m1 with
+          | None => print_term B;; failwith "Should never happen"
+          | Some H => let idB := reduce RedWhd (match H in _ = T' return B -> T' with
+                                 | eq_refl => fun (x : _) => x
+                                 end) in
+                              munify F idB UniNormal;; ret tt
+                          end;;
+                               print_term B;; print_term F;;
+        print "NoFun case";;
+              let A_red := reduce RedWhd  A in
+                         args <- args_of_max nindx A_red;
+                           atele <- get_ATele it args;
+                           a' <- @coerce _ (selem_of (ITele_App (isort := isort) atele)) a ;
+                             ret (cBase atele a')
 end.
 
-Definition get_CTele_SType := Eval compute in @get_CTele_raw SType.
-Definition get_CTele_SProp := Eval compute in @get_CTele_raw SProp.
-
-Definition get_CTele :=
-  fun isort =>
+Polymorphic Definition get_CTele :=
+  fun {isort} =>
     match isort as sort return forall {it : ITele sort} nindx {A : stype_of sort}, selem_of A -> M (CTele it) with
-    | SProp => get_CTele_SProp
-    | SType => get_CTele_SType
+    | SProp => get_CTele_raw (isort := SProp)
+    | SType => get_CTele_raw (isort := SType)
     end.
 
-Example get_RTrue_CTele := Eval compute in ltac:(mrun (get_CTele_SType reflect_itele 1 _ (RTrue True))).
-Example get_RFalse_CTele := Eval compute in ltac:(mrun (get_CTele_SType reflect_itele _ (RFalse True))).
+Example get_RTrue_CTele := Eval compute in ltac:(mrun (get_CTele reflect_itele 1 _ (RTrue True))).
+Example get_RFalse_CTele := Eval compute in ltac:(mrun (get_CTele reflect_itele 1 _ (RFalse True))).
 
 (* Record sdyn sort : Type := *)
 (*   { sdyn_type : stype_of sort; sdyn_elem : selem_of sdyn_type }. *)
 (* Arguments sdyn_type [_] _. *)
 (* Arguments sdyn_elem [_] _. *)
 
-Definition coerce_bla {S : Type} (F : S -> Type) s s' (t : F s) : M (F s') :=
-  oEqTy <- munify (F s') (F s) UniNormal;
-    match oEqTy with
-    | Some EqTy => match EqTy in _ = T return T -> M (F s') with
-                   | eq_refl => fun t => ret t
-                   end t
-    | None => raise CantCoerce
-    end.
+
+Definition sort_dyn (isort : Sort) A (a : A) : M (sigT (@selem_of isort)) :=
+    P <- @coerce _ (stype_of isort) A;
+    p <- @coerce _ (selem_of P) a;
+    ret ((existT _ _ p)).
+
+Polymorphic Definition sort_goal (A : Type) : M (sigT stype_of) :=
+  mtry P <- @coerce _ (stype_of SProp) A;
+       ret (existT _ SProp P)
+  with | _ =>
+    ret (existT _ SType A)
+           end.
+
+Polymorphic Definition map' := map.
+Arguments map' [_ _] _ _.
+
+Set Printing Universes.
+
+Definition dyn_of_stype {sort} : stype_of sort -> dyn :=
+  match sort with
+  | SProp => fun s => Dyn (selem_of s)
+  | SType => fun s => Dyn (selem_of s)
+  end.
+
+Unset Strict Universe Declaration.
+Polymorphic Definition uni_coerce : forall (A : Type@{i}) (B : Type@{j}), A -> M B :=
+fun A B (x : A) =>
+oH <- munify A B UniNormal;
+match oH with
+| Some H => retS coerce_rect B H x
+| None => raise CantCoerce
+end.
+
+
+
+Definition get_ind {A : Type} (n : A) :
+  M (nat * sigT (fun s => (ITele s)) * list dyn) :=
+  r <- constrs A;
+    print_term r;;
+               let (indP, constrs) := r in
+               sortit <- get_ITele (elem indP) : M (nat * sigT ITele);
+                 print_term sortit;;
+                            let nindx : nat := fst sortit in
+                            let (isort, it) := snd sortit in
+                            ret (nindx, existT _ _ it, constrs)
+.
+
+        (* Compute ind type ATele *)
+
+Definition get_ind_atele {isort} (it : ITele isort) (nindx : nat) (A : Type) :=
+    indlist <- args_of_max nindx A : M (list dyn);
+      atele <- get_ATele it indlist : M (ATele it);
+      ret atele.
 
 Definition new_destruct {A : Type} (n : A) : tactic :=
   fun g=>
-    r <- constrs n;
-      let (indP, constrs) := r in
-      sortit <- get_ITele indP;
-        let (isort, it) := sortit in
-        cts <- mmap (fun c_dyn =>
-                       let F sort := dyn -> @sigT (stype_of sort) (fun sty => selem_of sty) in
-                       let t : F SType := fun d => @existT (stype_of SType) (fun sty => selem_of sty) (type d) (elem d) in
-                       c_sorted <- coerce_bla _ SType isort t;
-                         let (ty, el) := c_sorted c_dyn in
-                         get_CTele _ it ty el
+    ind <- get_ind n;
+      let (nsortit, constrs) := ind in
+      let (nindx, sortit) := nsortit in
+      let (isort, it) := sortit in
+      atele <- get_ind_atele it nindx A;
+                 (* Compute CTeles *)
+        cts <- mmap (fun c_dyn : dyn =>
+                       c <- sort_dyn isort (type c_dyn) (elem c_dyn);
+                         let (ty, el) := c in
+                         get_CTele it nindx ty el
                     ) constrs;
-          print_term cts;;
-          ret [].
+                     (* Compute return type RTele *)
+        gt <- goal_type g;
+        rsG <- sort_goal gt;
+        let (rsort, sG) := rsG in
+        n' <- coerce n;
+          rt <- abstract_goal atele sG n';
+          sg <- mmap (
+                        fun ct =>
+                           @coerce _ Type  (selem_of (get_type_of_branch rt ct))
+                                  ) cts;
+          print "before type coercion";;
+          let h'' := map Dyn sg in
+          ret (map dyn_to_goal h'')
+.
 
+
+Set Printing All.
+Example test P b (r : reflect P b) : P <-> b = true :=
+  ltac:(
+    mrun ((fun g =>
+             t <- new_destruct r g;
+               print "debuggery";;
+               print_term t;;
+                          ret [g]) : tactic)
+).
 
 
   b <- is_var n;
