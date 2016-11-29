c186424d7a8b33fd8738b1bc0eabbd34f3b1ccc7
Beta Ziliani <bziliani@famaf.unc.edu.ar>
2016-11-29 19:29:44 -0300

    now intro_pattern is a list---what morally was anyway

--- tests/move.v
+++ tests/move.v
@@ -2,38 +2,24 @@ Require Import MetaCoq.
 Require Import Strings.String.
 
 
-Inductive intro_pattern : Type :=
-| ipnil : intro_pattern
-| ipcons : intro_base -> intro_pattern -> intro_pattern
-with intro_base :=
+Inductive intro_base :=
 | ipsimpl : intro_base
 | ipdone : intro_base
 | ipsimpldone : intro_base
 | ipmaybevar : intro_base
 | ipvar : string -> intro_base
 | ipfold : string -> intro_base
-| ipcase : list intro_pattern -> intro_base.
+| ipcase : list (list intro_base) -> intro_base.
 
+Definition intro_pattern := list intro_base.
 
 Section Parser.
 
 Notation "a >>= b" := (match a with None => None | Some a' => b a' end) (at level 100).
 
-Fixpoint token (s : string) :=
-  match s with
-  | EmptyString => ("", s)
-  | String "/" _ => ("", s)
-  | String "[" _ => ("", s)
-  | String "]" _ => ("", s)
-  | String " " s' => ("", s')
-  | String a s' =>
-    let (b, c) := token s' in
-    (String a b, c)
-  end.
-
 Definition remove_hd_maybevar r :=
   match r with
-  | ipcons ipmaybevar r' => r'
+  | (ipmaybevar :: r') => r'
   | _ => r
   end.
 
@@ -44,78 +30,78 @@ Fixpoint to_ip' (s : string) (hd : intro_pattern) {struct s} :=
   | String "/" s1 =>
     match s1 with
     | EmptyString => None
-    | String "=" s2 => to_ip' s2 (ipcons ipsimpl hd')
+    | String "=" s2 => to_ip' s2 (ipsimpl :: hd')
     | String "/" s2 =>
       match s2 with
       | String "=" s3 =>
-        to_ip' s3 (ipcons ipsimpldone hd')
+        to_ip' s3 (ipsimpldone :: hd')
       | _ =>
-        to_ip' s2 (ipcons ipdone hd')
+        to_ip' s2 (ipdone :: hd')
       end
     | _ =>
-      to_ip' s1 (ipcons (ipfold "") hd')
+      to_ip' s1 (ipfold "" :: hd')
     end
   | String " " s1 =>
     match hd with
-    | ipcons (ipfold "") _ => None
-    | ipcons (ipvar _) _ => to_ip' s1 (ipcons ipmaybevar hd)
-    | ipcons (ipfold _) _ => to_ip' s1 (ipcons ipmaybevar hd)
+    | ipfold "" :: _ => None
+    | ipvar _ :: _ => to_ip' s1 (ipmaybevar :: hd)
+    | ipfold _ :: _ => to_ip' s1 (ipmaybevar :: hd)
     | _ => to_ip' s1 hd
     end
   | String a s1 =>
     match hd with
-    | ipcons (ipfold b) c => to_ip' s1 (ipcons (ipfold (append b (String a ""))) c)
-    | ipcons (ipvar b) c => to_ip' s1 (ipcons (ipvar (append b (String a ""))) c)
-    | ipcons ipmaybevar b => to_ip' s1 (ipcons (ipvar (String a "")) b)
-    | _ => to_ip' s1 (ipcons (ipvar (String a "")) hd')
+    | ipfold b :: c => to_ip' s1 (ipfold (append b (String a "")) :: c)
+    | ipvar b :: c => to_ip' s1 (ipvar (append b (String a "")) :: c)
+    | ipmaybevar :: b => to_ip' s1 (ipvar (String a "") :: b)
+    | _ => to_ip' s1 (ipvar (String a "") :: hd')
     end
   end.
 
-Fixpoint ipapp a b :=
-  match a with
-  | ipnil => b
-  | ipcons x y => ipcons x (ipapp y b)
-  end.
-
-Fixpoint iprev i :=
-  match i with
-  | ipcons a b => ipapp (iprev b) (ipcons a ipnil)
-  | _ => i
-  end.
-
 Definition to_ip x :=
-  to_ip' x ipnil >>= (fun r=> Some (iprev r)).
+  to_ip' x [] >>= (fun r=> Some (rev r)).
 
-Example test1 : to_ip "x" = Some (ipcons (ipvar "x") ipnil).
+Example test1 : to_ip "x" = Some [ipvar "x"].
 Proof. reflexivity. Qed.
 
-Example test2 : to_ip "x y" = Some (ipcons (ipvar "x") (ipcons (ipvar "y") ipnil)).
+Example test2 : to_ip "x y" = Some [ipvar "x"; ipvar "y"].
 Proof. reflexivity. Qed.
 
-Example test3 : to_ip "xy" = Some (ipcons (ipvar "xy") ipnil).
+Example test3 : to_ip "xy" = Some [ipvar "xy"].
 Proof. reflexivity. Qed.
 
-Example test4 : to_ip "/xy" = Some (ipcons (ipfold "xy") ipnil).
+Example test4 : to_ip "/xy" = Some [ipfold "xy"].
 Proof. reflexivity. Qed.
 
-Example test5 : to_ip "w /xy" = Some (ipcons (ipvar "w") (ipcons (ipfold "xy") ipnil)).
+Example test5 : to_ip "w /xy" = Some [ipvar "w"; ipfold "xy"].
 Proof. reflexivity. Qed.
 
-Example test6 : to_ip "//" = Some (ipcons ipdone ipnil).
+Example test6 : to_ip "//" = Some [ipdone].
 Proof. reflexivity. Qed.
 
-Example test7 : to_ip "// " = Some (ipcons ipdone ipnil).
+Example test7 : to_ip "// " = Some [ipdone].
 Proof. reflexivity. Qed.
 
-Example test8 : to_ip "//=" = Some (ipcons ipsimpldone ipnil).
+Example test8 : to_ip "//=" = Some [ipsimpldone].
 Proof. reflexivity. Qed.
 
-Example test9 : to_ip "// b" = Some (ipcons ipdone (ipcons (ipvar "b") ipnil)).
+Example test9 : to_ip "// b" = Some [ipdone; ipvar "b"].
 Proof. reflexivity. Qed.
 
-Example test10 : to_ip "a // b" = Some (ipcons (ipvar "a") (ipcons ipdone (ipcons (ipvar "b") ipnil))).
+Example test10 : to_ip "a // b" = Some [ipvar "a"; ipdone; ipvar "b"].
 Proof. reflexivity. Qed.
 
 Example test_ultimate : to_ip "/abc x // IH /=" =
-  Some (ipcons (ipfold "abc") (ipcons (ipvar "x") (ipcons ipdone (ipcons (ipvar "IH") (ipcons ipsimpl ipnil))))).
+  Some [ipfold "abc"; ipvar "x"; ipdone; ipvar "IH"; ipsimpl].
 Proof. reflexivity. Qed.
+
+End Parser.
+
+Ltac done := intros; tauto || trivial || assumption || reflexivity.
+
+Definition done := ltac "Top.done" [].
+(*
+Definition build_tac ip :=
+  List.map
+  match ip with
+  |
+*)
