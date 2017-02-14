1afb4bb0216dd7f1756c8e4ff74600195236e505
Robbert Krebbers <mail@robbertkrebbers.nl>
2017-02-14 15:37:23 +0100

    Optimize match_goal'.
    Instead of snoc-ing items to the list (which is O(n)), cons them
    (which is O(1)) and at the end reverse the list.

--- theories/Tactics.v
+++ theories/Tactics.v
@@ -554,11 +554,11 @@ Definition match_goal' : goal_pattern -> list Hyp -> list Hyp -> tactic :=
     match oeqCA with
     | Some eqCA =>
       let a' := rcbv match eq_sym eqCA with eq_refl => a end in
-      mtry match_goal' (f a') [] (l1++l2')%list g
+      mtry match_goal' (f a') [] (List.rev_append l1 l2')%list g
       with DoesNotMatchGoal =>
-        match_goal' p (l1++[ahyp a d])%list l2' g
+        match_goal' p (ahyp a d :: l1) l2' g
       end
-    | None => match_goal' p (l1++[ahyp a d])%list l2' g end
+    | None => match_goal' p (ahyp a d :: l1) l2' g end
   | _, _ => raise DoesNotMatchGoal
   end.
 
