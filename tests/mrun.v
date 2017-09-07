5bc10cef773fa7d9a5861f8997a1b90294ce0811
Beta Ziliani <bziliani@famaf.unc.edu.ar>
2017-09-07 16:30:28 -0300

    a missing test on mrun

---
+++ tests/mrun.v
@@ -0,0 +1,23 @@
+From MetaCoq Require Import MetaCoq.
+
+Example trivial : True.
+Proof.
+  mrun (M.ret I).
+Qed.
+
+Example exact : True.
+Proof.
+  mrun (T.exact I).
+Qed.
+
+Example destruct : nat -> True.
+Proof.
+  mrun (T.destructn 0).
+  - mrun (T.exact I).
+  - mrun (T.introsn 1;; T.exact I).
+Qed.
+
+Example destruct2 : nat -> True.
+Proof.
+  mrun (T.destructn 0); [mrun (T.exact I) | mrun (T.introsn 1;; T.exact I)].
+Qed.
