7073e57e8348e6d4b63d4eca0e186af1d3fda32a
Beta Ziliani <bziliani@famaf.unc.edu.ar>
2017-09-19 16:09:28 -0300

    added test reproducing bug #71

--- tests/decompose.v
+++ tests/decompose.v
@@ -1,37 +1,28 @@
-From Mtac2 Require Import List Mtac2.
+From Mtac2 Require Import List Mtac2 Debugger.
 Import ListNotations.
 
-Definition decompose {A} (x : A) :=
+Definition decompose {T} (x : T) :=
   (mfix2 f (d : dyn) (args: list dyn) : M (dyn * list dyn) :=
+    M.print_term d;;
     mmatch d with
     | [? A B (t1: A -> B) t2] Dyn (t1 t2) => f (Dyn t1) [m: Dyn t2 & args]
     | [? A B (t1: forall x:A, B x) t2] Dyn (t1 t2) => f (Dyn t1) [m: Dyn t2 & args]
     | _ => M.ret (d, args)
     end) (Dyn x) [m:].
 
-Goal True.
-MProof.
-d <- decompose (M.ret I);
-match d with
-| (Dyn r, _) => mif M.unify_cumul M.ret r UniCoq then T.exact I else T.raise exception
-end.
-Qed.
-
 Import M.notations.
 
-Definition debug :=
-  M.break (fun A (x:A) =>
-             v <- decompose x;
-               let (hd, _) := v in
-               M.print_term hd;;
-             mif M.unify (Dyn (@M.ret)) hd UniCoq then
-               M.ret x
-             else M.raise exception) (M.ret I);; M.ret I.
+(* If I'm not mistaken, the problem comes from the unification of
 
-Import Mtac2.List.ListNotations.
+      ?t1 ?t2 =?= M.ret True
 
-
-Definition test : True.
-MProof.
-  Fail debug . (* it should unify with @M.ret, but it isn't because decompose returns [@M.ret True] *)
+   Which first tries to unify ?t1 with M.ret, without the knoweldge of
+   it being restricted by the second argument. I think it should first
+   unify ?t2 and then ?t1 (or delay the unification. *)
+Goal True.
+  MProof.
+   Fail mmatch Dyn (@M.ret True) with
+   | [? (P : Type -> Prop) (t1 : forall X:Type, P X) (t2 : Prop) ]  @Dyn (P t2) (t1 t2) => M.ret I
+    | _ => M.raise exception
+  end.
 Abort.
