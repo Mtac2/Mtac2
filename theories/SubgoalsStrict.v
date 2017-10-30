6304b19918f2f162867cdc8e09dd7b540dbf67bc
Beta Ziliani <beta@mpi-sws.org>
2017-10-30 15:42:24 -0300

    minor changes: now max_apply as variation of apply that will always introduce every hypotheses. it should also be faster.

--- theories/SubgoalsStrict.v
+++ theories/SubgoalsStrict.v
@@ -1,6 +1,17 @@
 From Mtac2 Require Import Base Tactics List.
+Import ListNotations.
 Set Implicit Arguments.
 
+(** This is a simple example tighting up a bit the types of tactics in order to
+    ensure a property.  In this case, we make sure that a variation of `apply`
+    is composed with as many tactics as the number required by the number of
+    hypotheses of the type. *)
+
+(** We won't statically check that the tactic isn't cheating. If the tactic
+    produce a different number of subgoals, that is not our problem here. *)
+
+(** For that reason, we use the following record instead of a Vector.t: we want
+    to easily embed tactics into ntactics and back. *)
 Record PackedVec (A: Type) (count: nat) := mkPackedVec {
   goals : list (A * goal)
 }.
@@ -12,20 +23,49 @@ Import M.notations.
 
 Coercion n_to_g A n (nt : ntactic A n) : gtactic A := fun g=>pv <- nt g; M.ret pv.(goals).
 
+(** For the composition, we can't be generic here: we produce a gtactic out of
+    the composition of an ntactic with nth gtactics. *)
 Class NSeq (A B : Type) n (nt: ntactic A n) (l: list (gtactic B)) (pf: length l = n) :=
   nseq : gtactic B.
 Arguments nseq {A B _} _%tactic _%tactic _ {_}.
 
 Import Mtac2.List.
+
 Instance nseq_list {A B} n (nt: ntactic A n) (l: list (gtactic B)) pf: NSeq nt l pf := fun g =>
   gs <- nt g;
   ls <- T.gmap l (map snd gs.(goals));
-  let res := dreduce (List.concat, List.app) (concat ls) in
+  let res := dreduce (concat, app) (concat ls) in
   T.filter_goals res.
 
 Notation "t1 '&n>' ts" :=
   (nseq t1 ts eq_refl) (at level 41, left associativity) : tactic_scope.
 
+Import Datatypes.
+
+(** [max_apply t] applies theorem t to the current goal.
+    It generates a subgoal for each non-dependent hypothesis in the theorem. *)
+Definition max_apply {T} (c : T) : tactic := fun g=>
+  match g with Goal eg =>
+    (mfix1 go (d : dyn) : M (list (unit * goal)) :=
+      (* let (_, el) := d in *)
+      (* mif M.unify_cumul el eg UniCoq then M.ret [m:] else *)
+        mmatch d return M (list (unit * goal)) with
+        | [? T1 T2 f] @Dyn (T1 -> T2) f =>
+          e <- M.evar T1;
+          r <- go (Dyn (f e));
+          M.ret ((tt, Goal e) :: r)
+        | [? T1 T2 f] @Dyn (forall x:T1, T2 x) f =>
+          e <- M.evar T1;
+          r <- go (Dyn (f e));
+          M.ret r
+        | Dyn eg => M.ret [m:]
+        | _ =>
+          let (ty, _) := d in
+          gT <- M.goal_type g;
+          M.raise (CantApply ty gT)
+        end) (Dyn c)
+  | _ => M.raise NotAGoal
+  end.
 
 Definition count_nondep_binders (T: Type) : M nat :=
   (mfix1 go (T : Type) : M nat :=
@@ -40,15 +80,28 @@ Definition count_nondep_binders (T: Type) : M nat :=
     end) T.
 
 Definition napply {T} {e: runner (count_nondep_binders T)} (c : T) : ntactic unit (@eval _ _ e) := fun g=>
-  ls <- T.apply c g;
+  ls <- max_apply c g;
   M.ret (mkPackedVec (@eval _ _ e) ls).
+
+
 Import T.
 Import T.notations.
-Import ListNotations.
+
 Goal forall P Q, (P -> Q -> P) -> P -> Q -> P.
 MProof.
   intros P Q H x y.
 Fail napply H &n> [m: assumption].
 Fail napply H &n> [m: ].
   napply H &n> [m: assumption | assumption].
+Qed.
+
+Goal forall P Q, (P -> Q -> P) -> P -> Q -> P.
+MProof.
+  intros P Q H x.
+Fail napply H &n> [m: assumption].
+Fail napply H &n> [m: ].
+  pose (tac := napply H &n> [m: assumption | assumption]).
+Fail T. (* ok, we can define the tactic, but it now fails to apply *)
+  intro y.
+  tac.
 Qed.
