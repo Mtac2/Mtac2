6a05dde1670e07f18805f6373e80343432aeb2b5
Robbert Krebbers <mail@robbertkrebbers.nl>
2017-11-06 14:08:41 +0100

    Export ListNotations; without that, it's barely possible to write any tactic.

--- theories/Mtac2.v
+++ theories/Mtac2.v
@@ -1,5 +1,7 @@
 From Mtac2 Require Export Base Tactics ImportedTactics IntroPatt.
+From Mtac2 Require Import List.
 
 Export M.notations.
 Export T.notations.
+Export ListNotations.
 Open Scope string_scope.
