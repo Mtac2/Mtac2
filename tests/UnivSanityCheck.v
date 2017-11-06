f1e579227caaf924c74a25c0d2a7868ef0b071a9
Beta Ziliani <beta@mpi-sws.org>
2017-11-06 16:39:37 -0300

    erasing test file generated with the universes

--- tests/UnivSanityCheck.v
+++ tests/UnivSanityCheck.v
@@ -1,14 +1,29 @@
 From Mtac2 Require Import Mtac2.
 
-Print Universes "univ.txt".
+Print Universes "universes-mtac2.txt".
 Import M.
 Require Import Coq.Numbers.BinNums.
 Require Import Strings.String.
 
-Definition find_cmd := "egrep ""Coq.*Mtac2.*"" univ.txt".
+Definition file := "universes-mtac2.txt".
+Definition magic_number := "3".
+
+(* we look for whaterever universes from Coq is < or <= than one of Mtac's *)
+Definition find_cmd := "egrep ""Coq.*Mtac2"" " ++ file.
+
+(* we count the lines (we get one for each pair of universes found above).
+   we also remove spaces since in Mac Os (apparently) wc returns spaces. *)
 Definition count_cmd := find_cmd ++ " | wc -l  | tr -d ' '".
-Definition assert_cmd := "[ $(" ++ count_cmd ++ ") = ""3"" ]".
+
+(* we test the result of the previous command to be equal to the actual number of
+   universes we expect to be in the list. currenlty, only those from ex *)
+Definition assert_cmd := "[ $(" ++ count_cmd ++ ") = """ ++ magic_number ++ """ ]".
 
 Goal eval (os_cmd assert_cmd) = Z0.
+  reflexivity.
+Qed.
+
+
+Goal eval (os_cmd ("rm " ++ file)) = Z0.
   reflexivity.
 Qed.
