992201055b23a3c19d9516b852bd38be2016146b
Beta Ziliani <bziliani@famaf.unc.edu.ar>
2018-01-07 15:48:17 -0300

    forgot a file in previous commit

--- theories/Ttactics.v
+++ theories/Ttactics.v
@@ -167,18 +167,18 @@ Definition compi {A} {B} (g : M A) (h : M B) : M (A * B) :=
 (** A typed assumption tactic *)
 Set Print Universes.
 Open Scope pattern_scope.
-Polymorphic Definition find@{i j k k1 k2 k3 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29 a30 a31} {A:Type@{i}} :=
+Polymorphic Definition find@{i j k k1 k2 k3 a1 a2 a3 a4 a5 a6 a7 a8} {A:Type@{i}} :=
   mfix1 f (l : mlist@{j} Hyp@{k}) : M A :=
-    mmatch'@{a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29 a30 a31} l (mcons@{j}
+    mmatch'@{a1 a2 a3 a4 a5 a6 a7 a8} l (mcons@{j}
       ([? x d (l': mlist@{j} Hyp@{k})] (mcons@{j} (@ahyp@{k} A x d) l') => M.ret x)
       (mcons@{j} ([? (ah:Hyp@{k}) (l': mlist@{j} Hyp@{k})] (mcons@{j} ah l') => f l')
       (mcons@{j} ([? l': mlist@{j} Hyp@{k}] l' => M.raise@{k1 k2 k3} exception)
        mnil@{j})))
     .
 
-Polymorphic Definition assumption@{i j k k1 k2 k3 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29 a30 a31} {A:Type@{i}} : M.t@{a3 j k} A :=
+Polymorphic Definition assumption@{i j k k1 k2 k3 a1 a2 a3 a4 a5 a6 a7 a8} {A:Type@{i}} : M.t@{a3 j k} A :=
   l <- hyps;
-  @find@{i j k k1 k2 k3 a1 a2 a3 a4 a5 a6 a7 a8 a9 a10 a11 a12 a13 a14 a15 a16 a17 a18 a19 a20 a21 a22 a23 a24 a25 a26 a27 a28 a29 a30 a31} A l.
+  @find@{i j k k1 k2 k3 a1 a2 a3 a4 a5 a6 a7 a8} A l.
 
 (** Solves goal A provided tactic t *)
 Definition by' {A} (t: tactic) : M A :=
