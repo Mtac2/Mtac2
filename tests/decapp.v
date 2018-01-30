52df8e05844631f74d5d8e4f192667ff72ba7571
Beta Ziliani <beta@mpi-sws.org>
2018-01-30 11:06:27 -0300

    executable notation for decompose app without requiring unicoq

---
+++ tests/decapp.v
@@ -0,0 +1,11 @@
+From Mtac2 Require Import Base DecomposeApp.
+
+Mtac Do Check ltac:(mrun (MTele_of (nat) _ (@plus))).
+
+Definition pairs := <[ decapp (3 + 5) @plus ]> (fun x y => M.ret (x,y)).
+Definition pairs_eq : M.eval pairs = (3, 5) := eq_refl.
+
+Fail Definition should_fail := <[ decapp (String.append "a" "b") @plus ]> (fun x y => M.ret (x,y)).
+
+Definition dyns := <[ decapp (Dyn 5) @Dyn ]> (fun ty el => M.ret ty).
+Definition dyns_eq : M.eval dyns = nat := eq_refl.
--- theories/DecomposeApp.v
+++ theories/DecomposeApp.v
@@ -7,8 +7,6 @@ Set Polymorphic Inductive Cumulativity.
 Set Universe Polymorphism.
 Unset Universe Minimization ToSet.
 
-Set Use Unicoq.
-
 (** Given `(A T : Type) (t : T)`, derive
     - `m : MTele`
     - `MTele_Const A m`
@@ -17,6 +15,7 @@ Set Use Unicoq.
     - `t : MTele_Const A m`
 *)
 
+(* FIX: proper error messaging *)
 Definition MTele_of (A : Type) : forall T, T -> M (msigT (MTele_Const (s:=SType) A)) :=
   let matchtele := fun T => mTele (fun _ : T => mBase) in
   let fixtele := mTele (matchtele) in
@@ -25,7 +24,6 @@ Definition MTele_of (A : Type) : forall T, T -> M (msigT (MTele_Const (s:=SType)
     (fun T (_ : T) => msigT (MTele_Const (s:=SType) A))
     (
       fun f T =>
-        (* ltac:(simpl in *; refine (mtmmatch' _ matchtele _ _ _)) *)
         mtmmatch'
           _
           matchtele
@@ -50,52 +48,29 @@ Definition MTele_of (A : Type) : forall T, T -> M (msigT (MTele_Const (s:=SType)
                                    let Fx := rone_step (F x) in
                                    let tx := (* rone_step *) (t x) in
                                    ''(mexistT _ n T) <- f Fx tx;
-                                   (* M.print "x";; *)
-                                   (* M.print_term x;; *)
-                                   (* M.print "n";; *)
-                                   (* M.print_term n;; *)
                                    n' <- M.abs_fun (P:=fun _ => MTele) x n;
-                                   (* M.print "n'";; *)
-                                   (* M.print_term n';; *)
-                                   (* M.print "T";; *)
-                                   (* M.print_term T;; *)
                                    T' <- M.coerce T;
                                    T' <- M.abs_fun (P:=fun x => MTele_Const (s:=SType) A (n' x)) x T';
-                                   (* T' <- M.abs_fun x T; *)
-                                   (* T' <- M.coerce T'; *)
                                    M.ret (mexistT (MTele_Const (s:=SType) A) (mTele n') T')
                                 )
                 )
                 UniCoq
-                                                 )
-                      )
-             )
+             )))
           ]
     ).
 
-Check ltac:(mrun (MTele_of (nat) _ (@plus))).
-
 (* This wrapper hardcodes an empty list of arguments. FIXME *)
-Definition decompose_app {m : MTele} {A B T: Type} (a : A) (t : T) {F}:
-  @lift
-    (MTele_Const (s:=SProp) (M B) m -> M B)
-    (
-      ''(mexistT _ m' T') <-
-        mtry
-          MTele_of A T t
-        with | [?E] E => M.print_term E;; M.evar _ end;
-        M.unify m m' UniCoq;;
-        (* M.print_term m;; *)
-        (* M.print_term T';; *)
-        (* let typi := dreduce (@MTele_Ty, @MTele_Sort) (MTele_Ty m) in *)
-        (* M.print_term typi;; *)
-        (* MT <- M.coerce (B:=typi) T'; *)
-                MT <- M.evar _;
-        t' <- M.coerce t;
-        eq <- M.unify_or_fail UniCoq _ _;
-        let x := @M.decompose_app' A B m (pBase) a MT t' eq in
-        M.ret x
-    )
-    (F) := F.
+Definition decompose_app {m : MTele} {A B T: Type} (a : A) (t : T):
+  M (MTele_Const (s:=SProp) (M B) m -> M B) :=
+  (
+    ''(mexistT _ m' T') <- MTele_of A T t;
+    M.unify m m' UniCoq;;
+    MT <- M.evar _;
+    t' <- M.coerce t;
+    eq <- M.unify_or_fail UniCoq _ _;
+    let x := @M.decompose_app' A B m (pBase) a MT t' eq in
+    M.ret x
+  ).
 
-Check ltac:(mrun ((decompose_app (3+5) (@plus) : (_ -> _)) (fun x y => M.ret (x,y)))).
+Notation "<[ 'decapp' a b ]>" := (ltac:(mrun (decompose_app a b)))
+  (at level 0, a at next level, b at next level).
