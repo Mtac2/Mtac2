e10da5dcbd004217495e02b9c49ec9b055b50ac8
Jan-Oliver Kaiser <janno@mpi-sws.org>
2018-02-14 17:13:07 +0100

    Fix syntactically broken testcases in decapp.v.

--- tests/decapp.v
+++ tests/decapp.v
@@ -1,15 +1,16 @@
 From Mtac2 Require Import Base DecomposeApp.
+Import M.notations.
 
 Mtac Do Check ltac:(mrun (MTele_of (nat) _ (@plus))).
 
-Definition pairs := <[decapp (3 + 5) @plus ]> (fun x y => M.ret (x,y)).
+Definition pairs := <[decapp (3 + 5) with @plus ]> UniMatchNoRed (fun x y => M.ret (x,y)).
 Definition pairs_eq : M.eval pairs = (3, 5) := eq_refl.
 
-Fail Definition should_fail := <[decapp (String.append "a" "b") @plus ]> (fun x y => M.ret (x,y)).
+Fail Definition should_fail := <[decapp (String.append "a" "b") with @plus ]> (fun x y => M.ret (x,y)).
 
-Definition dyns := <[decapp (Dyn 5) @Dyn ]> (fun ty el => M.ret ty).
+Definition dyns := <[decapp (Dyn 5) with @Dyn ]> UniMatchNoRed (fun ty el => M.ret ty).
 Definition dyns_eq : M.eval dyns = nat := eq_refl.
 
 
-Definition dyns_ty := <[decapp (Dyn 5) @Dyn with nat ]> (fun el => M.ret el).
+Definition dyns_ty := <[decapp (Dyn 5) with @Dyn nat ]> UniMatchNoRed (fun el => M.ret el).
 Definition dyns_ty_eq : M.eval dyns_ty = 5 := eq_refl.
