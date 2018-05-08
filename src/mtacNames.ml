e16c3670df9e5540ba5aa11bda78105469b7afec
Jan-Oliver Kaiser <janno@mpi-sws.org>
2018-05-08 15:06:36 +0200

    Fix some paths of Mtac constructs.

--- src/mtacNames.ml
+++ src/mtacNames.ml
@@ -15,17 +15,17 @@ let mkConstr e = ConstrBuilder.to_coq (ConstrBuilder.from_string (metaCoq_module
 let mkUConstr e sigma env = UConstrBuilder.to_coq (UConstrBuilder.from_string (metaCoq_module_name ^ "." ^ e)) sigma env
 
 let mkCase ind v ret branch sigma env =
-  let c = UConstrBuilder.from_string "Case.mkCase" in
+  let c = mkUBuilder "Case.mkCase" in
   UConstrBuilder.build_app c sigma env [|ind;v;ret;branch|]
 
 let mkelem d sigma env =
-  let c = UConstrBuilder.from_string "Dyn.elem" in
+  let c = mkUBuilder "Dyn.elem" in
   UConstrBuilder.build_app c sigma env [|d|]
 
-let mkdyn sigma env = UConstrBuilder.to_coq (UConstrBuilder.from_string "Dyn.dyn") sigma env
+let mkdyn sigma env = UConstrBuilder.to_coq (mkUBuilder "Dyn.dyn") sigma env
 
 let mkDyn ty el sigma env =
-  let c = UConstrBuilder.from_string "Dyn.Dyn" in
+  let c = mkUBuilder "Dyn.Dyn" in
   UConstrBuilder.build_app c sigma env [|ty;el|]
 
 (* dyn is expected to be Dyn ty el *)
