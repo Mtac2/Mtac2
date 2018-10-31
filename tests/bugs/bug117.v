6db857d25a99ea9ee4ac473bd12dbfd3f43a1253
beta <beta@mpi-sws.org>
2018-10-31 14:33:16 -0300

    solved #117 nice error message for when the list of goals is not normalized

---
+++ tests/bugs/bug117.v
@@ -0,0 +1,16 @@
+From Mtac2 Require Import Mtac2.
+
+Definition to_any (g: goal gs_base) : goal gs_any :=
+  match g with
+  | Goal s e => GoalOut s e
+  end.
+
+(* besides duplicating the same goal (which is not incorrect, just stupid)
+   this tactic is not normalizing the list. *)
+Definition wrong_tactic : tactic := \tactic g =>
+  M.ret ([m: (m: tt, to_any g)]+m+[m: (m: tt, to_any g)]).
+
+Goal True.
+MProof.
+  Fail wrong_tactic. (* it should fail nicely (without anomaly) *)
+Abort.
