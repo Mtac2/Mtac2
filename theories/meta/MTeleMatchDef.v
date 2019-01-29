99cca9785a1f1d2e0417f44228c0a8186ecbaceb
Jan-Oliver Kaiser <janno@mpi-sws.org>
2019-01-29 16:57:23 +0100

    Avoid `MTele_In` in `mtmmatch`; use `curry_..`.
    Also clean up the implementation significantly.

--- theories/meta/MTeleMatchDef.v
+++ theories/meta/MTeleMatchDef.v
@@ -4,87 +4,64 @@ Import Sorts.S.
 Import ListNotations.
 
 Set Universe Polymorphism.
+Set Polymorphic Inductive Cumulativity.
 Unset Universe Minimization ToSet.
 
 
-Inductive mtpattern A (m : A -> Prop)  : Prop :=
-| mtpbase : forall x : A, m x -> Unification -> mtpattern A m
-| mtptele : forall {C}, (forall x : C, mtpattern A m) -> mtpattern A m
-| mtpsort : (Sort -> mtpattern A m) -> mtpattern A m.
+Inductive mtpattern A (M : A -> Prop)  : Prop :=
+| mtpbase : forall x : A, M x -> Unification -> mtpattern A M
+| mtptele : forall {C}, (forall x : C, mtpattern A M) -> mtpattern A M
+| mtpsort : (Sort -> mtpattern A M) -> mtpattern A M.
 
 
-Arguments mtpbase {A m} _ _.
-Arguments mtptele {A m C} _.
-Arguments mtpsort {A m} _.
+Arguments mtpbase {A M} _ _.
+Arguments mtptele {A M C} _.
+Arguments mtpsort {A M} _.
 
 
 Local Notation MFA T := (MTele_val (MTele_C SType SProp M T)).
 
-Polymorphic Definition mtmmatch' A m (T : forall x, MTele_Ty (m x)) (y : A)
+Fixpoint open_branch {A} {m} {T : forall x, MTele_Ty (m x)} {y : A} {a : ArgsOf (m y)}
+         (p : mtpattern A (fun x => MFA (T x))) : M (apply_sort (T y) a) :=
+  match p return M _ with
+  | mtpbase x f u =>
+    oeq <- M.unify x y u;
+    match oeq return M (apply_sort (T y) a) with
+    | mSome eq =>
+      match eq in meq _ z return forall a : ArgsOf (m z), M (apply_sort (T z) a) with
+      | meq_refl => apply_C SProp f
+      end a
+    | mNone => M.raise DoesNotMatch
+    end
+  | mtptele f => c <- M.evar _; open_branch (f c)
+  | mtpsort f =>
+    M.mtry'
+      (open_branch (f SProp))
+      (fun e =>
+         oeq <- M.unify e DoesNotMatch UniMatchNoRed;
+         match oeq with
+         | mSome _ => open_branch (f SType)
+         | mNone => M.raise e
+         end
+      )
+  end
+.
+
+Definition mtmmatch' A m (T : forall x, MTele_Ty (m x)) (y : A)
            (ps : mlist (mtpattern A (fun x => MFA (T x)))) : selem_of (MFA (T y)) :=
-  MTele_open
-    M.t (T y)
-    (fun R acc =>
-       (fix mmatch' ps : M.t R :=
+  curry_C
+    SProp
+    (fun a : ArgsOf (m y) =>
+       (fix mmatch' (ps : mlist (mtpattern A (fun x => MFA (T x)))) :=
           match ps with
           | [m:] => M.raise NoPatternMatches
           | p :m: ps' =>
-            (* M.print "dbg2";; *)
-                    let g := (fix go p :=
-                                (* M.print "inner";; *)
-                                (* M.print_term p;; *)
-                        match p return M.t _ with
-                        | mtpbase x f u =>
-                          (* M.print "mtpbase";; *)
-                          oeq <- M.unify x y u;
-                          match oeq return M.t R with
-                          | mSome eq =>
-                            (* eq has type x = t, but for the pattern we need t = x.
-         we still want to provide eq_refl though, so we reduce it *)
-                            let h := dreduce (meq_sym) (meq_sym eq) in
-                            let 'meq_refl := eq in
-                            (* For some reason, we need to return the beta-reduction of the pattern, or some tactic fails *)
-
-                            (* M.print "dbg1";; *)
-                            let f' := (match h in _ =m= z return MTele_ty M.t (T z) -> MTele_ty M.t (T y)
-                                       with
-                                       | meq_refl => fun f => f
-                                       end f)
-                            in
-                            let a := acc _ f' in
-                            let b := reduce (RedStrong [rl:RedBeta]) (a) in
-                            (* b *)
-                            b
-                          | mNone =>
-                            M.raise DoesNotMatch
-                        end
-
-                        | mtptele f =>
-                          (* M.print "dbg3";; *)
-                          c <- M.evar _;
-                          go (f c)
-                        | mtpsort f =>
-                          M.mtry'
-                            (go (f SProp))
-                            (fun e =>
-                              oeq <- M.unify e DoesNotMatch UniMatchNoRed;
-                              match oeq with
-                              | mSome _ => go (f SType)
-                              | mNone => M.raise e
-                              end
-                            )
-                        end
-                     ) in
-            (* M.print_term p;; *)
-            let t := g p in
-            M.mtry' t
-                  (fun e =>
-                     mif M.unify e DoesNotMatch UniMatchNoRed then mmatch' ps' else M.raise e)
-            (* mtry' (open_mtpattern _ (fun _ => _)) *)
-            (*       (fun e => *)
-            (*          mif unify e DoesNotMatch UniMatchNoRed then mmatch' ps' else raise e) *)
+            M.mtry' (open_branch p)
+                    (fun e =>
+                       mif M.unify e DoesNotMatch UniMatchNoRed then mmatch' ps' else M.raise e)
           end) ps
     ).
+
 
 Module TestFin.
 Require Fin.
