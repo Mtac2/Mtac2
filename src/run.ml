502b1ba92edd1f1aa65143e5c23c1f28e9581bd7
Gaëtan Gilbert <gaetan.gilbert@skyskimmer.net>
2021-08-04 11:02:28 +0200

    Adapt to coq/coq#14705 (hints.ml has its own locality type instead of reusing option locality)

--- src/run.ml
+++ src/run.ml
@@ -2253,7 +2253,7 @@ and primitive ctxt vms mh reduced_term =
       let prio = CoqOption.from_coq sigma env (to_econstr prio) in
       let open Typeclasses in
       let hint_priority = Option.map (CoqN.from_coq (env, sigma)) prio in
-      let global = if global then Goptions.OptGlobal else Goptions.OptLocal in
+      let global = if global then Hints.SuperGlobal else Hints.Local in
       Classes.existing_instance global qualid (Some {hint_priority; hint_pattern= None});
       ereturn sigma (CoqUnit.mkTT)
   | MConstr (Minstantiate_evar, (ty, _, evar, solution, succ, fail)) ->
